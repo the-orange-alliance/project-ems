43ff51a671c29803d689900583219c940e88be2b
Kyle Flynn <kyleflynn.dev@gmail.com>
2022-03-29 19:34:12 -0400

    working antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import 'antd/dist/antd.css';
 import './index.less';
 import App from './App';
 
@@ -7,8 +8,5 @@ const container = document.getElementById('root');
 if (!container) throw new Error('Error while trying to find document root.');
 const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// NOTE: Currently, antd does not support React.StrictMode, so we have to live without it for now...
+root.render(<App />);
