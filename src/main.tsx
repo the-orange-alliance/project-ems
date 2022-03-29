2ec4eeb7e6cf5f04018586ff3698351e83e0f111
Kyle Flynn <kyleflynn.dev@gmail.com>
2022-03-29 13:39:38 -0400

    Added less and prettier/eslint/editorconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.less';
+import App from './App';
 
-const container = document.getElementById("root");
-if (!container) throw new Error("Error while trying to find document root.");
+const container = document.getElementById('root');
+if (!container) throw new Error('Error while trying to find document root.');
 const root = createRoot(container);
 
 root.render(
