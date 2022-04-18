1e46053d9269a6cd0a5ac34452d252e80711a807
Kyle Flynn <kyle.flynn5798@yahoo.com>
2022-04-17 22:27:35 -0400

    More project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react';
-import { Button, DatePicker, version } from 'antd';
+import { Button } from '@mui/material';
 import './App.less';
 
 function App() {
   return (
     <div>
-      <h1>antd version: {version}</h1>
-      <DatePicker />
-      <Button type='primary' style={{ marginLeft: 8 }}>
+      <Button color='primary' variant='contained'>
         Primary Button
       </Button>
     </div>
