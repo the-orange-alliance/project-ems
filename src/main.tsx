ccb1dac55ab09dd016b9dbc05aaded7167d592cf
Kyle Flynn <kyle.flynn5798@yahoo.com>
2022-04-17 22:45:56 -0400

    Updated container root

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.less';
 import App from './App';
 
-const container = document.getElementById('test');
+const container = document.getElementById('root');
 if (!container) throw new Error('Error while trying to find document root.');
 const root = createRoot(container);
 
