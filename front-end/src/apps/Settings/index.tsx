8d8651aa8e07fe03dc4b7eedb061c7476b5c7db8
Kyle Flynn <kyleflynn.dev@gmail.com>
2022-07-19 17:55:28 -0400

    Renamed

--- front-end/src/apps/Settings/index.tsx
+++ front-end/src/apps/Settings/index.tsx
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import { FormControlLabel, FormGroup, Switch } from '@mui/material';
 import { useRecoilState } from 'recoil';
-import { darkModeAtom } from '../../stores/recoil';
+import { darkModeAtom } from '../../stores/Recoil';
 
 const SettingsApp: FC = () => {
   const [darkMode, setDarkMode] = useRecoilState(darkModeAtom);
