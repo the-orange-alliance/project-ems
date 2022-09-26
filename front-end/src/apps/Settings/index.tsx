a487e8968e09929d070801a519f3509cdaaff59d
Kyle Flynn <kyle.flynn5798@yahoo.com>
2022-09-26 00:06:36 -0400

    configurable motor duration

--- front-end/src/apps/Settings/index.tsx
+++ front-end/src/apps/Settings/index.tsx
@@ -1,18 +1,24 @@
-import { FC } from 'react';
+import { FC, ChangeEvent } from 'react';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import DefaultLayout from 'src/layouts/DefaultLayout';
 import { FormControlLabel, FormGroup, Switch } from '@mui/material';
 import { useRecoilState } from 'recoil';
-import { darkModeAtom } from 'src/stores/Recoil';
+import { darkModeAtom, fieldMotorDuration } from 'src/stores/Recoil';
 
 const SettingsApp: FC = () => {
   const [darkMode, setDarkMode] = useRecoilState(darkModeAtom);
+  const [motorDuration, setMotorDuration] = useRecoilState(fieldMotorDuration);
 
   const changeDarkMode = (): void => {
     setDarkMode(!darkMode);
+  };
+
+  const changeMotorDuration = (event: ChangeEvent<HTMLInputElement>) => {
+    setMotorDuration(parseInt(event.target.value));
   };
 
   return (
@@ -35,6 +41,29 @@ const SettingsApp: FC = () => {
               label={
                 <Typography sx={{ marginRight: 'auto', fontWeight: 'bold' }}>
                   Dark Mode
+                </Typography>
+              }
+              labelPlacement='start'
+              sx={{ padding: (theme) => theme.spacing(2) }}
+            />
+          </FormGroup>
+          <FormGroup
+            sx={{
+              '&:hover': {
+                backgroundColor: (theme) => theme.palette.action.hover
+              }
+            }}
+          >
+            <FormControlLabel
+              control={
+                <TextField
+                  value={motorDuration}
+                  onChange={changeMotorDuration}
+                />
+              }
+              label={
+                <Typography sx={{ marginRight: 'auto', fontWeight: 'bold' }}>
+                  Field Preparation Motor Duration
                 </Typography>
               }
               labelPlacement='start'
