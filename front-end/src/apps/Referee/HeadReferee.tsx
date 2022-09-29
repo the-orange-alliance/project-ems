b4bc0a0d1e1ccd27f8b2804e1e0c38472f56216c
jfabellera <jfabellera@gmail.com>
2022-09-29 17:18:25 -0500

    Add per team storage level and cards
    Add card functionality

--- front-end/src/apps/Referee/HeadReferee.tsx
+++ front-end/src/apps/Referee/HeadReferee.tsx
@@ -1,13 +1,16 @@
 import { FC, useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { useSocket } from 'src/api/SocketProvider';
 import DefaultLayout from 'src/layouts/DefaultLayout';
-import { loadedMatchKey } from 'src/stores/Recoil';
+import { loadedMatchKey, matchInProgress } from 'src/stores/Recoil';
 import { Box } from '@mui/material';
 import RefereeSheet from './components/games/CarbonCapture/RefereeSheet';
 import ScoreSheetSmall from './components/games/CarbonCapture/ScoreSheetSmall';
 
 const HeadReferee: FC = () => {
+  const match = useRecoilValue(matchInProgress);
+  const redAlliance = match?.participants?.filter((p) => p.station < 20);
+  const blueAlliance = match?.participants?.filter((p) => p.station >= 20);
   const [, setMatchKey] = useRecoilState(loadedMatchKey);
   const [socket, connected] = useSocket();
 
@@ -29,8 +32,8 @@ const HeadReferee: FC = () => {
     <DefaultLayout containerWidth='xl'>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
         <Box sx={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
-          <RefereeSheet alliance='red' />
-          <RefereeSheet />
+          <RefereeSheet alliance={redAlliance} />
+          <RefereeSheet alliance={blueAlliance} />
         </Box>
         <ScoreSheetSmall />
       </Box>
--- front-end/src/apps/Referee/components/games/CarbonCapture/RefereeSheet.tsx
+++ front-end/src/apps/Referee/components/games/CarbonCapture/RefereeSheet.tsx
@@ -1,25 +1,174 @@
-import { FC } from 'react';
-import { Box, Paper, Typography } from '@mui/material';
-import StorageLevelInput from './StorageLevelInput';
-import PenaltyInput from './PenaltyInput';
+import { FC, useState, MouseEvent, useEffect } from 'react';
+import {
+  Box,
+  Paper,
+  Typography,
+  ToggleButtonGroup,
+  ToggleButton
+} from '@mui/material';
+import {
+  matchInProgress,
+  matchInProgressParticipantByKey
+} from 'src/stores/Recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { MatchParticipant } from '@toa-lib/models';
+import NumberInput from '../../NumberInput';
+import { useSocket } from 'src/api/SocketProvider';
 
-const RefereeSheet: FC<{ alliance?: string }> = ({ alliance }) => {
+const TeamSection: FC<{ participantKey: string }> = ({ participantKey }) => {
+  const [participant, setParticipant] = useRecoilState(
+    matchInProgressParticipantByKey(participantKey)
+  );
+  const match = useRecoilValue(matchInProgress);
+  const [updateReady, setUpdateReady] = useState(false);
+  const [socket] = useSocket();
+  const [storageLevel, setStorageLevel] = useState(0);
+
+  useEffect(() => {
+    if (updateReady) {
+      setUpdateReady(false);
+      socket?.emit('match:update', match);
+    }
+  }, [updateReady]);
+
+  const handleStorageLevel = (
+    event: MouseEvent<HTMLElement>,
+    newStorageLevel: number
+  ) => {
+    if (newStorageLevel !== null) {
+      setStorageLevel(newStorageLevel);
+    }
+  };
+
+  const handleCardChange = (newValue: number) => {
+    if (participant) {
+      const newParticipant = Object.assign({}, participant);
+      newParticipant.cardStatus = newValue;
+      setParticipant(newParticipant);
+      setUpdateReady(true);
+    }
+  };
+
+  const handleYellowCard = () => {
+    if (participant?.cardStatus == 1) {
+      handleCardChange(0);
+    } else {
+      handleCardChange(1);
+    }
+  };
+
+  const handleRedCard = () => {
+    if (participant?.cardStatus == 2) {
+      handleCardChange(0);
+    } else {
+      handleCardChange(2);
+    }
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '8px',
+        alignItems: 'center'
+      }}
+    >
+      <Typography variant='h6'>
+        {participant?.team?.teamNameLong} ({participant?.team?.country})
+      </Typography>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '8px',
+          width: '100%'
+        }}
+      >
+        <ToggleButtonGroup
+          fullWidth
+          color='primary'
+          value={storageLevel}
+          onChange={handleStorageLevel}
+          exclusive
+        >
+          <ToggleButton value={0}>0</ToggleButton>
+          <ToggleButton value={1}>1 PL</ToggleButton>
+          <ToggleButton value={2}>2 LO</ToggleButton>
+          <ToggleButton value={3}>3 MD</ToggleButton>
+          <ToggleButton value={4}>4 HI</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '8px',
+          width: '100%',
+          height: '2em'
+        }}
+      >
+        <ToggleButton
+          fullWidth
+          value='yellowCard'
+          selected={participant?.cardStatus == 1}
+          onChange={handleYellowCard}
+          color='warning'
+        >
+          Yellow Card
+        </ToggleButton>
+        <ToggleButton
+          fullWidth
+          value='redCard'
+          selected={participant?.cardStatus == 2}
+          onChange={handleRedCard}
+          color='error'
+        >
+          Red Card
+        </ToggleButton>
+      </Box>
+    </Box>
+  );
+};
+
+const RefereeSheet: FC<{ alliance: MatchParticipant[] }> = ({ alliance }) => {
   return (
     <Paper
       sx={{
         padding: (theme) => theme.spacing(2),
         borderStyle: 'solid',
         borderWidth: 'thick',
-        borderColor: alliance == 'red' ? '#de1f1f' : '#1f85de',
+        borderColor: alliance?.some((p) => p.station < 20)
+          ? '#de1f1f'
+          : '#1f85de',
         width: '100%'
       }}
     >
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-        <Typography variant='h4' sx={{ textAlign: 'center' }}>
-          {alliance == 'red' ? 'Red' : 'Blue'} Alliance
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+        <Typography variant='h5' sx={{ textAlign: 'center' }}>
+          {alliance?.some((p) => p.station < 20) ? 'Red' : 'Blue'} Alliance
         </Typography>
-        <StorageLevelInput />
-        <PenaltyInput />
+        {alliance?.map((p) => (
+          <TeamSection
+            key={p.matchParticipantKey}
+            participantKey={p.matchParticipantKey}
+          />
+        ))}
+
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '8px',
+            width: '100%',
+            alignItems: 'center'
+          }}
+        >
+          <Typography variant='h6'>Fouls</Typography>
+          <NumberInput value={0} />
+        </Box>
       </Box>
     </Paper>
   );
