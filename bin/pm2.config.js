bf35ccaff7a39c4cc09dc14099ab7ed53bb7645f
Kyle Flynn <kyle.flynn5798@yahoo.com>
2022-10-11 11:58:50 +0200

    Fixed pm2 config

--- bin/pm2.config.js
+++ bin/pm2.config.js
@@ -1,15 +1,21 @@
 module.exports = {
   apps : [{
     name   : "EMS API",
-    script : "./api/Server.js",
+    script : "./api/index.js",
     env: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      SERVICE_NAME: "ems-relatime",
+      SERVICE_PORT: 8080,
+      JWT_SECRET: 'toaadmin1'
     }
   },{
     name   : "EMS REALTIME",
-    script : "./api/Server.js",
+    script : "./realtime/index.js",
     env: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      SERVICE_NAME: "ems-relatime",
+      SERVICE_PORT: 8081,
+      JWT_SECRET: 'toaadmin1'
     }
   }]
 }
