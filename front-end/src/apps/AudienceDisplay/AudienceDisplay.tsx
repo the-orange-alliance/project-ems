35ee2dee05d2ea40f726aa657df5ec5b9f06c613
NoahAndrews <noah@revrobotics.com>
2023-09-05 12:47:18 -0500

    Hide the cursor when it's motionless in the Audience Display app

--- front-end/src/apps/AudienceDisplay/AudienceDisplay.tsx
+++ front-end/src/apps/AudienceDisplay/AudienceDisplay.tsx
@@ -36,6 +36,26 @@ const AudienceDisplay: FC = () => {
     };
   }, [display]);
 
+  useEffect(() => {
+    let mouseStoppedMovingTimeout: number = -1;
+    const hideCursor = (): void => {
+      document.body.style.cursor = 'none';
+    };
+    const onMouseMove = (): void => {
+      // The cursor is being moved, display it for the next 500ms
+      document.body.style.cursor = '';
+      clearTimeout(mouseStoppedMovingTimeout);
+      mouseStoppedMovingTimeout = window.setTimeout(() => hideCursor(), 500);
+    };
+
+    hideCursor();
+    window.addEventListener('mousemove', onMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  });
+
   const onDisplay = (id: number) => {
     setDisplay(id);
   };
