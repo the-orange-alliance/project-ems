a642acdbaefbb4c8487cf0c3be156114f8e12302
Kyle Flynn <kyleflynn.dev@gmail.com>
2023-09-11 00:20:27 -0400

    Initial play display for stream

--- front-end/src/apps/AudienceDisplay/AudienceDisplay.tsx
+++ front-end/src/apps/AudienceDisplay/AudienceDisplay.tsx
@@ -1,5 +1,6 @@
 import { clientFetcher } from '@toa-lib/client';
 import { Displays, isMatch, Match, MatchKey } from '@toa-lib/models';
+import { useSearchParams } from 'react-router-dom';
 import { FC, ReactNode, useEffect } from 'react';
 import { useRecoilCallback, useRecoilState, useRecoilValue } from 'recoil';
 import { useSocket } from 'src/api/SocketProvider';
@@ -16,11 +17,14 @@ import MatchPlay from './displays/fgc_2023/MatchPlay/MatchPlay';
 import MatchResults from './displays/fgc_2023/MatchResults/MatchResults';
 import { useHiddenMotionlessCursor } from '@features/hooks/use-hidden-motionless-cursor';
 import './AudienceDisplay.less';
+import MatchResultsOverlay from './displays/fgc_2023/MatchResults/MatchResultsOverlay';
 
 const AudienceDisplay: FC = () => {
   const [display, setDisplay] = useRecoilState(displayIdAtom);
   const chromaKey = useRecoilValue(displayChromaKeyAtom);
   const [socket, connected] = useSocket();
+  const [searchParams] = useSearchParams();
+  const mode = searchParams.get('mode') ?? '';
   useHiddenMotionlessCursor();
 
   useEffect(() => {
@@ -56,7 +60,7 @@ const AudienceDisplay: FC = () => {
       <MatchStateListener />
       <PrestartListener />
       <div id='aud-base' style={{ backgroundColor: chromaKey }}>
-        {getDisplay(display)}
+        {getDisplay(display, mode)}
       </div>
     </ChromaLayout>
   );
@@ -64,17 +68,26 @@ const AudienceDisplay: FC = () => {
 
 export default AudienceDisplay;
 
-function getDisplay(id: number): ReactNode {
+function getDisplay(id: number, mode: string): ReactNode {
   switch (id) {
     case -1:
       return <div />;
     case Displays.MATCH_PREVIEW:
       return <MatchPreview />;
     case Displays.MATCH_START:
-      return <MatchPlay />;
+      return getPlayDisplay(mode);
     case Displays.MATCH_RESULTS:
       return <MatchResults />;
     default:
       return <div />;
+  }
+}
+
+function getPlayDisplay(mode: string): ReactNode {
+  switch (mode) {
+    case 'stream':
+      return <MatchResultsOverlay />;
+    default:
+      return <MatchPlay />;
   }
 }
