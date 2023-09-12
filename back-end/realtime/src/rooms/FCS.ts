68f7baa0a1935ab17848966736e475b59c21c9b1
NoahAndrews <noah@revrobotics.com>
2023-09-11 19:02:36 -0500

    When a new FCS client connects, send it the latest field state

--- back-end/realtime/src/rooms/FCS.ts
+++ back-end/realtime/src/rooms/FCS.ts
@@ -4,24 +4,26 @@ import Room from "./Room.js";
 import Match from "./Match.js";
 
 export default class FCS extends Room {
+  private readonly latestFcsStatus: FieldControlUpdatePacket = { hubs: {} };
+
   public constructor(server: Server, matchRoom: Match) {
     super(server, "fcs");
 
     matchRoom.localEmitter.on("match:start", () => {
-      this.broadcast().emit("fcs:update", FCS_MATCH_START);
+      this.broadcastFcsUpdate(FCS_MATCH_START);
     });
 
     matchRoom.localEmitter.on("match:endgame", () => {
-      this.broadcast().emit("fcs:update", FCS_ENDGAME);
+      this.broadcastFcsUpdate(FCS_ENDGAME);
     });
 
     matchRoom.localEmitter.on("match:end", () => {
       // TODO(Noah): Placeholder
-      this.broadcast().emit("fcs:update", FCS_IDLE);
+      this.broadcastFcsUpdate(FCS_IDLE);
     });
 
     matchRoom.localEmitter.on("match:abort", () => {
-      this.broadcast().emit("fcs:update", FCS_FIELD_FAULT);
+      this.broadcastFcsUpdate(FCS_FIELD_FAULT);
     })
   }
 
@@ -29,7 +31,58 @@ export default class FCS extends Room {
     socket.emit("fcs:init", FCS_INIT);
 
     socket.on("fcs:update", (update: FieldControlUpdatePacket) => {
-      this.broadcast().emit("fcs:update", update);
+      this.broadcastFcsUpdate(update);
     });
+
+    socket.emit("fcs:update", this.latestFcsStatus);
+  }
+
+  private broadcastFcsUpdate(update: FieldControlUpdatePacket): void {
+    this.broadcast().emit("fcs:update", update);
+
+    // Update this.latestFcsStatus AFTER sending out the new update
+    for (const hubNumber in update.hubs) {
+      const hubFromThisUpdate = update.hubs[hubNumber];
+
+      let latestFcsStatusHub = this.latestFcsStatus.hubs[hubNumber];
+      if (!latestFcsStatusHub) {
+        latestFcsStatusHub = {};
+        this.latestFcsStatus.hubs[hubNumber] = latestFcsStatusHub;
+      }
+
+      const servosFromThisUpdate = hubFromThisUpdate.servos;
+      const motorsFromThisUpdate = hubFromThisUpdate.motors;
+      const digitalInputsFromThisUpdate = hubFromThisUpdate.digitalInputs;
+
+      if (servosFromThisUpdate) {
+        for (const newServoParams of servosFromThisUpdate) {
+          // Remove any parameters from this.latestFcsStatus that are intended for the same servo as newServoParams.
+          latestFcsStatusHub.servos = latestFcsStatusHub.servos?.filter(
+            oldServoParams => oldServoParams.port != newServoParams.port) ?? [];
+
+          latestFcsStatusHub.servos!.push(newServoParams)
+        }
+      }
+
+      if (motorsFromThisUpdate) {
+        for (const newMotorParams of motorsFromThisUpdate) {
+          // Remove any parameters from this.latestFcsStatus that are intended for the same motor as newMotorParams.
+          latestFcsStatusHub.motors = latestFcsStatusHub.motors?.filter(
+            oldMotorParams => oldMotorParams.port != newMotorParams.port) ?? [];
+
+          latestFcsStatusHub.motors!.push(newMotorParams)
+        }
+      }
+
+      if (digitalInputsFromThisUpdate) {
+        for (const newDigitalInputParams of digitalInputsFromThisUpdate) {
+          // Remove any parameters from this.latestFcsStatus that are intended for the same channel as newDigitalInputParams.
+          latestFcsStatusHub.digitalInputs = latestFcsStatusHub.digitalInputs?.filter(
+            oldDigitalInputParams => oldDigitalInputParams.channel != newDigitalInputParams.channel) ?? [];
+
+          latestFcsStatusHub.digitalInputs!.push(newDigitalInputParams)
+        }
+      }
+    }
   }
 }
