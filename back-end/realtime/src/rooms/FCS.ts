112f7e144e654ee66a340fd34d3084899f687055
NoahAndrews <noah@revrobotics.com>
2023-09-14 18:41:27 -0500

    Turn off lights during countdown

--- back-end/realtime/src/rooms/FCS.ts
+++ back-end/realtime/src/rooms/FCS.ts
@@ -1,5 +1,13 @@
 import { Server, Socket } from "socket.io";
-import { FCS_ENDGAME, FCS_FIELD_FAULT, FCS_IDLE, FCS_INIT, FCS_MATCH_START, FieldControlUpdatePacket } from "@toa-lib/models";
+import {
+  FCS_ENDGAME,
+  FCS_FIELD_FAULT,
+  FCS_IDLE,
+  FCS_INIT,
+  FCS_MATCH_START,
+  FCS_TURN_OFF_LIGHTS,
+  FieldControlUpdatePacket
+} from "@toa-lib/models";
 import Room from "./Room.js";
 import Match from "./Match.js";
 
@@ -10,6 +18,10 @@ export default class FCS extends Room {
     super(server, "fcs");
 
     matchRoom.localEmitter.on("match:start", () => {
+      this.broadcastFcsUpdate(FCS_TURN_OFF_LIGHTS);
+    });
+
+    matchRoom.localEmitter.on("match:tele", () => {
       this.broadcastFcsUpdate(FCS_MATCH_START);
     });
 
