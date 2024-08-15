ae77b0894dbe3f9c2cdd3855c5658b82dc639260
Soren Zaiser <soren.zaiser@gmail.com>
2024-08-14 22:24:12 -0400

    Add 2024 SQL

---
+++ back-end/api/sql/seasons/fgc_2024.sql
@@ -0,0 +1,21 @@
+ALTER TABLE "ranking" ADD COLUMN rankingScore INT;
+ALTER TABLE "ranking" ADD COLUMN highestScore INT;
+ALTER TABLE "ranking" ADD COLUMN oxyHydroPoints INT;
+
+ALTER TABLE "match_detail" ADD COLUMN redResevoirConserved;
+ALTER TABLE "match_detail" ADD COLUMN redNexusConserved;
+ALTER TABLE "match_detail" ADD COLUMN redFoodProduced;
+ALTER TABLE "match_detail" ADD COLUMN redFoodSecured;
+ALTER TABLE "match_detail" ADD COLUMN redRobotOneBalanced;
+ALTER TABLE "match_detail" ADD COLUMN redRobotTwoBalanced;
+ALTER TABLE "match_detail" ADD COLUMN redRobotThreeBalanced;
+
+ALTER TABLE "match_detail" ADD COLUMN blueResevoirConserved;
+ALTER TABLE "match_detail" ADD COLUMN blueNexusConserved;
+ALTER TABLE "match_detail" ADD COLUMN blueFoodProduced;
+ALTER TABLE "match_detail" ADD COLUMN blueFoodSecured;
+ALTER TABLE "match_detail" ADD COLUMN blueRobotOneBalanced;
+ALTER TABLE "match_detail" ADD COLUMN blueRobotTwoBalanced;
+ALTER TABLE "match_detail" ADD COLUMN blueRobotThreeBalanced;
+
+ALTER TABLE "match_detail" ADD COLUMN coopertition;
