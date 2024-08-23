285f0b332a619854d7f57205ed912687e2b91b6c
Soren Zaiser <soren.zaiser@gmail.com>
2024-08-23 00:15:50 -0400

    bugfixes

--- front-end/src/layouts/chroma-layout.tsx
+++ front-end/src/layouts/chroma-layout.tsx
@@ -1,18 +1,21 @@
 import { FC, ReactNode } from 'react';
 import { Box } from '@mui/material';
+import { useRecoilValue } from 'recoil';
+import { displayChromaKeyAtom } from 'src/stores/recoil';
 
 interface Props {
   children?: ReactNode;
 }
 
 export const ChromaLayout: FC<Props> = ({ children }) => {
+  const chromaKey = useRecoilValue(displayChromaKeyAtom);
   return (
     <Box>
       {/* because mui is dumb */}
       <style>
         {`
           body {
-            background: #00000000
+            background: ${chromaKey}
           }
         `}
       </style>
@@ -21,7 +24,7 @@ export const ChromaLayout: FC<Props> = ({ children }) => {
         sx={{
           height: '100vh',
           width: '100vw',
-          overflow: 'hidden',
+          overflow: 'hidden'
         }}
       >
         {children}
