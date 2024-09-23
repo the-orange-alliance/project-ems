3aa5d760ca7abe7b8b49ac6c76318afc4b93dd5a
Kyle Flynn <kyle.flynn5798@yahoo.com>
2024-09-23 20:26:18 +0300

    Improved service logging

--- back-end/api/src/util/Logger.ts
+++ back-end/api/src/util/Logger.ts
@@ -1,13 +1,27 @@
-import winston from 'winston';
+import winston, { format } from 'winston';
+import { threadId } from 'worker_threads';
+
+const { combine, timestamp, label, printf, colorize } = format;
+
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `[${timestamp}][${level}][${label}]: ${message}`;
+});
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'api' }),
+    timestamp({ format: 'YYYY-MM-DD.HH:mm:ss' }),
+    colorize(),
+    customFormat
+  ),
+  defaultMeta: threadId ? { tid: `${threadId}` } : undefined,
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
+    new winston.transports.Console(),
     new winston.transports.File({
       filename: './logs/error.log',
       level: 'error'
@@ -15,11 +29,4 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: './logs/combined.log' })
   ]
 });
-
-logger.add(
-  new winston.transports.Console({
-    format: winston.format.simple()
-  })
-);
-
 export default logger;
--- back-end/realtime/src/util/Logger.ts
+++ back-end/realtime/src/util/Logger.ts
@@ -1,15 +1,27 @@
-import winston from 'winston';
+import winston, { format } from 'winston';
 import { threadId } from 'worker_threads';
+
+const { combine, timestamp, label, printf, colorize } = format;
+
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `[${timestamp}][${level}][${label}]: ${message}`;
+});
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'rlt' }),
+    timestamp({format: 'YYYY-MM-DD.HH:mm:ss'}),
+    colorize(),
+    customFormat
+  ),
   defaultMeta: threadId ? { tid: `${threadId}` } : undefined,
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
+    new winston.transports.Console(),
     new winston.transports.File({
       filename: './logs/error.log',
       level: 'error'
@@ -17,17 +29,4 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: './logs/combined.log' })
   ]
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple()
-    })
-  );
-}
-
 export default logger;
