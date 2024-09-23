7fa0f5a03448d920395ae4c8c7135a4bd0283f6d
Kyle Flynn <kyle.flynn5798@yahoo.com>
2024-09-23 13:02:45 +0300

    update script

--- scripts/update.py
+++ scripts/update.py
@@ -1,15 +1,36 @@
 import os
 import zipfile
+import requests
+from github import Github
+from github import Auth
 from pathlib import Path
+from dotenv import load_dotenv
 
-downloads_path = str(Path.home() / "Downloads")
-output_path = str(Path.home() / "Desktop" / "ems-latest")
-items = os.listdir(downloads_path)
-builds = list(filter(lambda item: 'ems.' in item, items))
-print(builds)
+load_dotenv("./../front-end/.env")
+github_token = os.getenv("VITE_GITHUB_TOKEN")
+auth=Auth.Token(github_token)
+git = Github(auth=auth)
+repo = git.get_repo("the-orange-alliance/project-ems")
+latest_release = repo.get_latest_release()
+download_url = latest_release.assets[0].browser_download_url
+print(f'Latest release: {latest_release.tag_name}')
+print(f'Downloading {download_url}...')
 
-input=int(input('Select a build: '))
-zip_path = str(downloads_path + '/' + builds[input])
+response = requests.get(download_url)
 
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+download_path = Path.home() / "Downloads" / "ems-latest.zip"
+output_path = Path.home() / "Desktop" / "fgc-2024"
+
+with open(download_path, 'wb') as f:
+    f.write(response.content)
+
+print('Done. Extracting...')
+
+with zipfile.ZipFile(download_path, 'r') as zip_ref:
     zip_ref.extractall(path=output_path)
+
+print("Done. Cleaning up...")
+
+os.remove(download_path)
+
+print("Done. Update complete.")
