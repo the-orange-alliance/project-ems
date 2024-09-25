15e8d2ef96a575391a022b67a6005418fefdb2c7
Soren Zaiser <soren.zaiser@gmail.com>
2024-09-25 07:19:25 -0400

    Fix the silly socket, but make it better too

--- front-end/src/layouts/chroma-layout.tsx
+++ front-end/src/layouts/chroma-layout.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useRecoilValue } from 'recoil';
-import { displayChromaKeyAtom } from 'src/stores/recoil';
+import { displayChromaKeyAtom, socketConnectedAtom } from 'src/stores/recoil';
 
 interface Props {
   children?: ReactNode;
@@ -9,8 +9,26 @@ interface Props {
 
 export const ChromaLayout: FC<Props> = ({ children }) => {
   const chromaKey = useRecoilValue(displayChromaKeyAtom);
+  const connected = useRecoilValue(socketConnectedAtom);
   return (
     <Box>
+      {!connected && (
+        <Typography
+          variant='h5'
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            textAlign: 'center',
+            color: 'black',
+            fontWeight: 'bold',
+            '-webkit-text-stroke': '.5px white'
+          }}
+        >
+          Socket Not Connected!
+        </Typography>
+      )}
       {/* because mui is dumb */}
       <style>
         {`
