0d356a7f22235409e1afe2bb594b63527b6aa765
Soren Zaiser <soren.zaiser@gmail.com>
2024-09-27 08:03:38 -0400

    fix many things in little code

--- front-end/src/apps/scorekeeper/hooks/use-prestart.ts
+++ front-end/src/apps/scorekeeper/hooks/use-prestart.ts
@@ -1,4 +1,4 @@
-import { useRecoilCallback } from 'recoil';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 import { useMatchControl } from './use-match-control';
 import {
   getDefaultMatchDetailsBySeasonKey,
@@ -6,6 +6,7 @@ import {
   MatchState
 } from '@toa-lib/models';
 import {
+  currentEventKeyAtom,
   currentTournamentKeyAtom,
   matchOccurringAtom,
   socketConnectedAtom
@@ -14,10 +15,13 @@ import { patchMatch, patchMatchParticipants } from 'src/api/use-match-data';
 import { DateTime } from 'luxon';
 import { once, sendPrestart, sendUpdate } from 'src/api/use-socket';
 import { useSeasonFieldControl } from 'src/hooks/use-season-components';
+import { useTeamsForEvent } from 'src/api/use-team-data';
 
 export const usePrestartCallback = () => {
   const { canPrestart, setState } = useMatchControl();
   const fieldControl = useSeasonFieldControl();
+  const event = useRecoilValue(currentEventKeyAtom);
+  const teams = useTeamsForEvent(event);
   return useRecoilCallback(
     ({ snapshot, set }) =>
       async () => {
@@ -40,11 +44,17 @@ export const usePrestartCallback = () => {
         if (prestartTime) {
           await patchMatch({ ...match, prestartTime });
         }
+        let currentMatch = { ...match };
+        currentMatch.participants = currentMatch.participants?.map((p) => ({
+          ...p,
+          team: p.team || teams.data?.find((t) => t.teamKey === p.teamKey)
+        }));
+
         await patchMatchParticipants(
           { eventKey, tournamentKey, id },
           match.participants
         );
-        let currentMatch = match;
+
         if (!match.details) {
           currentMatch = {
             ...match,
@@ -57,17 +67,19 @@ export const usePrestartCallback = () => {
               id: match.id
             }
           };
-          set(matchOccurringAtom, currentMatch);
-          set(currentTournamentKeyAtom, currentMatch.tournamentKey);
         }
+
+        set(matchOccurringAtom, currentMatch);
+        set(currentTournamentKeyAtom, currentMatch.tournamentKey);
         fieldControl?.prestartField?.();
         // Once we recieve the prestart response, immediately send update to load socket with match
+        console.log(currentMatch);
         once(MatchSocketEvent.PRESTART, () => sendUpdate(currentMatch));
         // Send prestart to server
         sendPrestart({ eventKey, tournamentKey, id });
         setState(MatchState.PRESTART_COMPLETE);
       },
-    [canPrestart, setState]
+    [canPrestart, setState, event, teams]
   );
 };
 
