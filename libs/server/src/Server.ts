2878f2d3ea5830bc7faf004470571e4cd091fd3e
Kyle Flynn <kyleflynn.dev@gmail.com>
2025-03-09 21:27:51 -0400

    fixed all vulnerability issues

--- libs/server/src/Server.ts
+++ libs/server/src/Server.ts
@@ -1,5 +1,5 @@
 export const getIPv4 = (): string => {
   // TODO: Make this configurable
   // See https://github.com/the-orange-alliance/project-ems/issues/75
-  return "0.0.0.0";
+  return '0.0.0.0';
 };
