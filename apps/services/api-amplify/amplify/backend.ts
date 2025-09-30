37764dc0ef6a97e83678ae480eac91198a19f5ce
Kyle Flynn <kyleflynn.dev@gmail.com>
2025-09-30 15:45:04 -0400

    initial api functions

--- apps/services/api-amplify/amplify/backend.ts
+++ apps/services/api-amplify/amplify/backend.ts
@@ -1,11 +1,53 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { storage } from "./storage/resource";
 import { auth } from "./auth/resource";
+import { apiEventsFunction } from "./functions/api-events/resource";
+import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { Stack } from "aws-cdk-lib";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   storage,
   auth,
+  apiEventsFunction,
+});
+
+const apiStack = backend.createStack("ems-api-stack");
+
+const restApi = new RestApi(apiStack, "rest-api", {
+  restApiName: "ems-rest-api",
+  deploy: true,
+  deployOptions: {
+    stageName: "dev", // TODO: make this configurable
+  },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_METHODS,
+    allowMethods: Cors.ALL_METHODS,
+    allowHeaders: Cors.DEFAULT_HEADERS,
+  },
+});
+
+const eventsLambdaIntegration = new LambdaIntegration(
+  backend.apiEventsFunction.resources.lambda,
+);
+
+const eventItemsPath = restApi.root.addResource("events");
+
+eventItemsPath.addMethod("GET", eventsLambdaIntegration);
+eventItemsPath.addMethod("POST", eventsLambdaIntegration);
+eventItemsPath.addMethod("PUT", eventsLambdaIntegration);
+eventItemsPath.addMethod("DELETE", eventsLambdaIntegration);
+
+backend.addOutput({
+  custom: {
+    API: {
+      [restApi.restApiName]: {
+        endpoint: restApi.url,
+        region: Stack.of(restApi).region,
+        apiName: restApi.restApiName,
+      },
+    },
+  },
 });
