4e992952b1189fc9979dc7f31eb6d52be0ca2684
Kyle Flynn <kyle.flynn5798@yahoo.com>
2025-10-02 11:07:47 -0400

    updated route visibility for production builds

--- apps/web/src/apps/index.tsx
+++ apps/web/src/apps/index.tsx
@@ -1,8 +1,8 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useMemo } from 'react';
 import { Row, Col, Divider } from 'antd';
 import { DefaultLayout } from '@layouts/default-layout.js';
 import { AppCard, AppCardProps, AppRow } from '@components/util/app-card.js';
-import AppRoutes from '../app-routes.js';
+import routes from '../app-routes.js';
 import { useCurrentEvent } from '@api/use-event-data.js';
 import { useUpdateAppbar } from 'src/hooks/use-update-appbar.js';
 
@@ -25,6 +25,14 @@ const AppCards: FC = () => {
 const App = () => {
   const { data: event, isLoading } = useCurrentEvent();
   useUpdateAppbar({ title: event ? event.eventName : undefined }, [event]);
+  const buildType = import.meta.env.VITE_BUILD_TYPE;
+  const filteredRoutes = useMemo(
+    () =>
+      routes.filter((route) =>
+        buildType === 'production' ? route.online !== false : true
+      ),
+    [routes, buildType]
+  );
   return (
     <>
       <Row gutter={[24, 24]} style={{ marginBottom: 16 }} justify={'center'}>
@@ -34,7 +42,8 @@ const App = () => {
           md={16}
           style={{ width: '100%', marginBottom: 16 }}
         >
-          {AppRoutes.filter((route) => route.eventOrder)
+          {filteredRoutes
+            .filter((route) => route.eventOrder)
             .sort((a, b) => (a.eventOrder ?? 0) - (b.eventOrder ?? 0))
             .map((route) => (
               <AppRow key={route.path} route={route} />
@@ -43,8 +52,9 @@ const App = () => {
       </Row>
       <Divider>Other Apps</Divider>
       <Row gutter={[24, 24]} style={{ marginBottom: 32 }}>
-        {AppRoutes.filter((route) => !route.hidden && !route.eventOrder).map(
-          (route, i) => (
+        {filteredRoutes
+          .filter((route) => !route.hidden && !route.eventOrder)
+          .map((route, i) => (
             <ColAppCard
               key={`route-${i}`}
               title={route.name}
@@ -52,8 +62,7 @@ const App = () => {
               imgSrc={route.image ? route.image : undefined}
               loading={isLoading}
             />
-          )
-        )}
+          ))}
       </Row>
     </>
   );
