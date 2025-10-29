61263319e8058920c3af868f6e7255c9503ff8af
Jan-Felix Abellera <jfabellera@gmail.com>
2025-10-29 15:53:04 -0500

    Refactor field monitor extra component

--- apps/web/src/seasons/fgc-2025/fieldMonitorExtra.tsx
+++ apps/web/src/seasons/fgc-2025/fieldMonitorExtra.tsx
@@ -23,6 +23,102 @@ export const StatusTag = ({
   </Tag>
 );
 
+type DispenserCardProps = {
+  name: string;
+  status: FGC25FCS.DispenserFcsStatus;
+};
+
+const DispenserCard: FC<DispenserCardProps> = ({ name, status }) => {
+  return (
+    <Flex vertical flex={1} gap='0.5rem'>
+      <Card>
+        <Typography.Text strong style={{ alignSelf: 'center' }}>
+          {name}
+        </Typography.Text>
+        <Flex justify='space-between'>
+          <span>Temp</span>
+          <span style={{ fontWeight: 500 }}>
+            {Number(status.temperature).toFixed(1)}
+            {' °C'}
+          </span>
+        </Flex>
+        <Flex justify='space-between'>
+          <span>Current</span>
+          <span style={{ fontWeight: 500 }}>
+            {Number(status.current).toFixed(1)}
+            {' A'}
+          </span>
+        </Flex>
+        <Flex justify='space-between'>
+          <span>Unjam Count</span>
+          <span style={{ fontWeight: 500 }}>
+            {Number(status.unjamCount).toFixed(0)}
+          </span>
+        </Flex>
+        <Flex justify='center'>
+          <StatusTag
+            status={status.indexerBeamBreak ? 'success' : 'error'}
+            label='Beam Break'
+          />
+        </Flex>
+      </Card>
+    </Flex>
+  );
+};
+
+type EcosystemCardProps = {
+  name: string;
+  status: FGC25FCS.EcosystemFcsStatus;
+};
+
+const EcosystemCard: FC<EcosystemCardProps> = ({ name, status }) => {
+  return (
+    <Flex vertical flex={1} gap='0.5rem'>
+      <Card>
+        <Typography.Text strong>{name}</Typography.Text>
+        <Flex flex={1} gap='0.5rem'>
+          <StatusTag
+            status={status.l3BeamBreak ? 'success' : 'error'}
+            label='L3'
+          />
+          <StatusTag
+            status={status.l2BeamBreak ? 'success' : 'error'}
+            label='L2'
+          />
+          <StatusTag
+            status={status.l1BeamBreak ? 'success' : 'error'}
+            label='L1'
+          />
+        </Flex>
+      </Card>
+    </Flex>
+  );
+};
+
+type AcceleratorCardProps = {
+  name: string;
+  status: FGC25FCS.AcceleratorFcsStatus;
+};
+
+const AcceleratorCard: FC<AcceleratorCardProps> = ({ name, status }) => {
+  return (
+    <Flex vertical flex={1} gap='0.5rem'>
+      <Card>
+        <Typography.Text strong style={{ alignSelf: 'center' }}>
+          {name}
+        </Typography.Text>
+        <Flex justify='space-between'>
+          <span>Velocity</span>
+          <span style={{ fontWeight: 500 }}>
+            {Number(status.velocity).toFixed(1)}
+            {' RPM'}
+          </span>
+        </Flex>
+      </Card>
+    </Flex>
+  );
+};
+
 export const FieldMonitorExtra: FC<FGC25FCS.FcsStatus> = (fcsStatus) => {
   return fcsStatus ? (
     <Card title='FCS Status' size='small' style={{ width: '100%' }}>
@@ -68,193 +164,36 @@ export const FieldMonitorExtra: FC<FGC25FCS.FcsStatus> = (fcsStatus) => {
 
         {/* Dispensers */}
         <Flex justify='space-between' gap='1rem'>
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong style={{ alignSelf: 'center' }}>
-                Blue Dispenser
-              </Typography.Text>
-              <Flex justify='space-between'>
-                <span>Temp</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.blueDispenser.temperature).toFixed(1)}
-                  {' °C'}
-                </span>
-              </Flex>
-              <Flex justify='space-between'>
-                <span>Current</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.blueDispenser.current).toFixed(1)}
-                  {' A'}
-                </span>
-              </Flex>
-              <Flex justify='space-between'>
-                <span>Unjam Count</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.blueDispenser.unjamCount).toFixed(0)}
-                </span>
-              </Flex>
-              <Flex justify='center'>
-                <StatusTag
-                  status={
-                    fcsStatus.blueDispenser.indexerBeamBreak
-                      ? 'success'
-                      : 'error'
-                  }
-                  label='Beam Break'
-                />
-              </Flex>
-            </Card>
-          </Flex>
-
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong style={{ alignSelf: 'center' }}>
-                Red Dispenser
-              </Typography.Text>
-              <Flex justify='space-between'>
-                <span>Temp</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.redDispenser.temperature).toFixed(1)}
-                  {' °C'}
-                </span>
-              </Flex>
-              <Flex justify='space-between'>
-                <span>Current</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.redDispenser.current).toFixed(1)}
-                  {' A'}
-                </span>
-              </Flex>
-              <Flex justify='space-between'>
-                <span>Unjam Count</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.redDispenser.unjamCount).toFixed(0)}
-                </span>
-              </Flex>
-              <Flex justify='center'>
-                <StatusTag
-                  status={
-                    fcsStatus.redDispenser.indexerBeamBreak
-                      ? 'success'
-                      : 'error'
-                  }
-                  label='Beam Break'
-                />
-              </Flex>
-            </Card>
-          </Flex>
+          <DispenserCard
+            name='Blue Dispenser'
+            status={fcsStatus.blueDispenser}
+          />
+          <DispenserCard name='Red Dispenser' status={fcsStatus.redDispenser} />
         </Flex>
 
         {/* Ecosystems */}
         <Flex justify='space-between' gap='1rem' style={{ width: '100%' }}>
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong>Blue Ecosystem</Typography.Text>
-              <Flex flex={1} gap='0.5rem'>
-                <StatusTag
-                  status={
-                    fcsStatus.blueEcosystem.l3BeamBreak ? 'success' : 'error'
-                  }
-                  label='L3'
-                />
-                <StatusTag
-                  status={
-                    fcsStatus.blueEcosystem.l2BeamBreak ? 'success' : 'error'
-                  }
-                  label='L2'
-                />
-                <StatusTag
-                  status={
-                    fcsStatus.blueEcosystem.l1BeamBreak ? 'success' : 'error'
-                  }
-                  label='L1'
-                />
-              </Flex>
-            </Card>
-          </Flex>
-
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong>Center Ecosystem</Typography.Text>
-              <Flex flex={1} gap='0.5rem'>
-                <StatusTag
-                  status={
-                    fcsStatus.centerEcosystem.l3BeamBreak ? 'success' : 'error'
-                  }
-                  label='L3'
-                />
-                <StatusTag
-                  status={
-                    fcsStatus.centerEcosystem.l2BeamBreak ? 'success' : 'error'
-                  }
-                  label='L2'
-                />
-                <StatusTag
-                  status={
-                    fcsStatus.centerEcosystem.l1BeamBreak ? 'success' : 'error'
-                  }
-                  label='L1'
-                />
-              </Flex>
-            </Card>
-          </Flex>
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong>Red Ecosystem</Typography.Text>
-              <Flex flex={1} gap='0.5rem'>
-                <StatusTag
-                  status={
-                    fcsStatus.redEcosystem.l3BeamBreak ? 'success' : 'error'
-                  }
-                  label='L3'
-                />
-                <StatusTag
-                  status={
-                    fcsStatus.redEcosystem.l2BeamBreak ? 'success' : 'error'
-                  }
-                  label='L2'
-                />
-                <StatusTag
-                  status={
-                    fcsStatus.redEcosystem.l1BeamBreak ? 'success' : 'error'
-                  }
-                  label='L1'
-                />
-              </Flex>
-            </Card>
-          </Flex>
+          <EcosystemCard
+            name='Blue Ecosystem'
+            status={fcsStatus.blueEcosystem}
+          />
+          <EcosystemCard
+            name='Center Ecosystem'
+            status={fcsStatus.centerEcosystem}
+          />
+          <EcosystemCard name='Red Ecosystem' status={fcsStatus.redEcosystem} />
         </Flex>
 
         {/* Accelerators */}
         <Flex justify='space-between' gap='1rem'>
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong style={{ alignSelf: 'center' }}>
-                Blue Accelerator
-              </Typography.Text>
-              <Flex justify='space-between'>
-                <span>Velocity</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.blueAccelerator.velocity).toFixed(1)}
-                  {' RPM'}
-                </span>
-              </Flex>
-            </Card>
-          </Flex>
-          <Flex vertical flex={1} gap='0.5rem'>
-            <Card>
-              <Typography.Text strong style={{ alignSelf: 'center' }}>
-                Red Accelerator
-              </Typography.Text>
-              <Flex justify='space-between'>
-                <span>Velocity</span>
-                <span style={{ fontWeight: 500 }}>
-                  {Number(fcsStatus.redAccelerator.velocity).toFixed(1)}
-                  {' RPM'}
-                </span>
-              </Flex>
-            </Card>
-          </Flex>
+          <AcceleratorCard
+            name='Blue Accelerator'
+            status={fcsStatus.blueAccelerator}
+          />
+          <AcceleratorCard
+            name='Red Accelerator'
+            status={fcsStatus.redAccelerator}
+          />
         </Flex>
       </Flex>
     </Card>
